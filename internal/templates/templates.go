50df1be1fe4cffff799bf9b33f8d6d24e38ddc25
Tomi Haapalainen <tomi.haapalainen@proton.me>
2024-12-13 15:16:52 +0200

    update

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -4,12 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
-	"io"
 	"io/fs"
 	"path/filepath"
 	"strings"
-
-	"github.com/labstack/echo/v4"
 )
 
 func NewRenderer() *Template {
@@ -57,10 +54,6 @@ func NewRenderer() *Template {
 
 type Template struct {
 	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 type Error struct {
